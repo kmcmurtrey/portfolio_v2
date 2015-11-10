98d96323deec3ae643b359caf72579177f1c19bf
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-11-09 22:47:12 -0500

    Creates basic form template

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -15,7 +15,6 @@
         </div>
         <div class="hero">
             <div class="hero-inner">
-                {{--<a href="" class="hero-logo"><img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="Logo Image"></a>--}}
                 <div class="hero-copy">
                     <p>My name is Korey McMurtrey, a Code Louisville graduate, and this is my portfolio.</p>
                 </div>
@@ -23,32 +22,65 @@
         </div>
     </header>
 
-<div class="main-container">
+    <div class="main-container">
 
-    <section class="project">
-        <img class="project-img" src="assets/img/php_project.jpg" alt="Screenshot of PHP demo project.">
-        <div class="project-content">
-            <h2>PHP Project</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <ul class="tech">
-                <li>PHP</li>
-                <li>MySQL</li>
-            </ul>
-        </div>
-    </section>
+        <section class="project">
+            <img class="project-img" src="assets/img/php_project.jpg" alt="Screenshot of PHP demo project.">
+            <div class="project-content">
+                <h2>PHP Project</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <ul class="tech">
+                    <li>PHP</li>
+                    <li>MySQL</li>
+                </ul>
+            </div>
+        </section>
 
-    <section class="project">
-        <img class="project-img" src="assets/img/lutes_dental.jpg" alt="Screenshot of Lutes Dental Office website.">
-        <div class="project-content">
-            <h2>Lutes Dental Office</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <ul class="tech">
-                <li>HTML/CSS</li>
-                <li>jQuery</li>
-                <li>Foundation</li>
-            </ul>
-        </div>
-    </section>
-</div>
+        <section class="project">
+            <img class="project-img" src="assets/img/lutes_dental.jpg" alt="Screenshot of Lutes Dental Office website.">
+            <div class="project-content">
+                <h2>Lutes Dental Office</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <ul class="tech">
+                    <li>HTML/CSS</li>
+                    <li>jQuery</li>
+                    <li>Foundation</li>
+                </ul>
+            </div>
+        </section>
+    </div>
+
+    {{--Form elements--}}
+    {!! Form::open(['url' => 'contact']) !!}
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name: ') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email', 'Email: ') !!}
+        {!! Form::text('email', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('message', 'Message: ') !!}
+        {!! Form::textarea('message', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('Send', ['class' => 'btn form-control']) !!}
+    </div>
+
+    {!! Form::close() !!}
+
+    @if ($errors->any())
+        <ul class="alert">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
 @stop
+
